822c65cc2893f6f62b7eb44a4109a811f44d1a3b
Maksym <150538968+Baromental@users.noreply.github.com>
2025-08-10 14:45:03 +0300

    title styles

--- src/components/TitleComponent/TitleComponent.jsx
+++ src/components/TitleComponent/TitleComponent.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 
+import s from './TitleComponent.module.css';
+
 export const TitleComponent = () => {
   return (
-    <div>
-      <h1>Test assignment for front-end developer</h1>
-      <p>
+    <div className={s.titleContainer}>
+      <h1 className={s.title}>Test assignment for front-end developer</h1>
+      <p className={s.paragraph}>
         What defines a good front-end developer is one that has skilled
         knowledge of HTML, CSS, JS with a vast understanding of User design
         thinking as they'll be building web interfaces with accessibility in
         mind. They should also be excited to learn, as the world of Front-End
         Development keeps evolving.
       </p>
-      <button>Sign up</button>
+      <button className={s.button}>Sign up</button>
     </div>
   );
 };
