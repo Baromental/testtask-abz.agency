571d8fc7418d0bb087bd2b0ddb7862349a1acda5
Maksym <150538968+Baromental@users.noreply.github.com>
2025-08-11 17:59:46 +0300

    get component styles

--- src/components/GetComponent/GetComponent.jsx
+++ src/components/GetComponent/GetComponent.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import s from './GetComponent.module.css';
 
 export const GetComponent = () => {
   return (
-    <div>
-      <h2>Working with GET request</h2>
-      <ul></ul>
-      <button>Show more</button>
+    <div className={s.getContainer}>
+      <h2 className={s.title}>Working with GET request</h2>
+      <ul className={s.list}></ul>
+      <button className={s.button}>Show more</button>
     </div>
   );
 };
